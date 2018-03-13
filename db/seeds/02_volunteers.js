da628d9ede5aacb9d430f3b08a87a8fccf12bb8c
Noel Serrato <Serrato@LGMBP.local>
2018-03-13 10:35:27 -0700

    NOEL - FIX VOLUNTEER

--- db/seeds/02_volunteers.js
+++ db/seeds/02_volunteers.js
@@ -4,7 +4,7 @@ exports.seed = function(knex, Promise) {
   return knex('volunteers').del()
     .then(function () {
       // Inserts seed entries
-      return knex('volunteer').insert([
+      return knex('volunteers').insert([
         {
           username: 'volunteers',
           pw: '123456'
